ed15ca1bcb1610c971eb002b103b0a87480bfd4a
Boris Goncharov <bora4bora@gmail.com>
2020-02-06 19:57:42 -0800

    profile interface

--- SwiftyCompanion/ProfileViewController.swift
+++ SwiftyCompanion/ProfileViewController.swift
@@ -11,12 +11,14 @@ import Alamofire
 import SwiftyJSON
 
 class ProfileViewController: UIViewController {
-
+    
     var json: JSON?
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var avatar: UIImageView!
     @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var skillsTable: UITableView!
+    @IBOutlet weak var projectsTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +28,37 @@ class ProfileViewController: UIViewController {
         avatar.layer.borderColor = UIColor.white.cgColor
         avatar.layer.cornerRadius = avatar.frame.width / 2
         backgroundView.backgroundColor = UIColor(patternImage: UIImage(named: "42_bg")!)
+        
+        skillsTable.layer.cornerRadius = 5
+        projectsTable.layer.cornerRadius = 5
     }
     
     func setProfileFields() {
         
     }
-
+    
     func setPhoto() {
         
     }
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if tableView == skillsTable, let cell = tableView.dequeueReusableCell(withIdentifier: "skillsCell", for: indexPath) as? SkillsTableViewCell {
+            cell.skillLabel.text = "Algorithms"
+            return cell
+        } else if tableView == projectsTable, let cell = tableView.dequeueReusableCell(withIdentifier: "projectsCell", for: indexPath) as? ProjectsTableViewCell {
+            cell.projectLabel.text = "Swifty Companion - 125.0%"
+            return cell
+        }
+        return UITableViewCell()
+    }
     
 }
+
---
+++ SwiftyCompanion/TableViewCells.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCells.swift
+//  SwiftyCompanion
+//
+//  Created by Boris on 2/6/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class SkillsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var skillLabel: UILabel!
+
+}
+
+class ProjectsTableViewCell: UITableViewCell {
+    @IBOutlet weak var projectLabel: UILabel!
+    
+}
